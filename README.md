e69e3221d1d23ed1c8fc812ca604ba1244448ab7
andrewwellercs <38194149+andrewwellercs@users.noreply.github.com>
2018-06-14 18:04:33 -0700

    Test Commit
    Hi

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 2dRallyExtreme
-A Racing Game
+# 2d Rally Extreme
+This is a racing game. But it's not really a game yet. There's a menu though.
--- src/RallyRunner.java
+++ src/RallyRunner.java
@@ -4,7 +4,7 @@ public class RallyRunner
 	public static void main(String[] args) throws InterruptedException
 	{
 		Game game = new Game();
-		
+		System.out.println("this is a test commit");
 		while (true)
 		{
 			game.update();
